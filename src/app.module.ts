d4515ea1c699a958588d89b7a08bf2483f9319e4
diegorrfc <diego.rrfc@gmail.com>
2020-06-21 00:53:39 -0300

    Inicio da implementção do ORM mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
 import { MongooseModule, } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { StoreModule } from './modules/store/store.module';
+
 
 @Module({
-  imports: [
-    HttpModule,
-    BackofficeModule,
+  imports: [  
     MongooseModule.forRoot(
       'mongodb://localhost:27017/nestApi',
       { useNewUrlParser: true }
     ),
+    TypeOrmModule.forRoot({     
+        type: 'mssql',
+        host: 'localhost',
+        port: 1433,
+        username: 'nestApi',
+        password: '##MS_PolicyEventProcessingLogin##',
+        database: 'NestApi',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      HttpModule,
+      BackofficeModule,
+      StoreModule,
   ],
   controllers: [],
   providers: [],
